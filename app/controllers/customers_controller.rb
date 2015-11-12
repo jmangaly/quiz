d9a1cf96857a2306eb3dc95b9fa1dd5369a06e29
Jenny Mangaly <jmangaly@gmail.com>
2015-11-12 05:21:13 +0000

    quiz2

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,25 @@
 class CustomersController < ApplicationController
 	def index
 		@customer = Customer.all
+	end
+
+	def show
+    	@customer = Customer.find(params[:id])
+    end
+ 
+ 	def new
+ 		@customer = Customer.new
+ 	end	
+
+ 	def create
+    	Customer.create(customer_params)
+    	redirect_to root_path
+  	end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:order, :phone)
+  end
 end
-end
+
